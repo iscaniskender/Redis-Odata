534f4ae7459e9064659c7c8013c36fd208f899e9
İskender İşcan <iscaniskender@gmail.com>
2022-10-05 00:10:17 +0300

    Finish

---
+++ RedisExchangeAPI.Web/Controllers/RedisStringType.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class RedisStringType : Controller
+    {
+        private readonly RedisService redisService;
+
+        private readonly IDatabase db;
+        public RedisStringType(RedisService redisService)
+        {
+            this.redisService = redisService;
+            db = redisService.GetDb(0);
+        }
+        public IActionResult Index()
+        {
+            db.StringSet("name", "iskender");
+            db.StringSet("visitor", 100);
+            return View();
+        }
+    }
+}
---
+++ RedisExchangeAPI.Web/Services/RedisService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Services
+{
+    public class RedisService
+    {
+        private readonly string redisHost;
+        private readonly string redisPort;
+        private ConnectionMultiplexer redis;
+        public IDatabase db { get;set; }    
+        public RedisService(IConfiguration configuration)
+        {
+            redisHost = configuration["Redis:Host"];
+            redisPort = configuration["Redis:Port"];
+        }
+        public void Connect()
+        {
+            var configString = $"{redisHost}:{redisPort}";
+
+            redis = ConnectionMultiplexer.Connect(configString);
+        }
+        public IDatabase GetDb(int db)
+        {
+            return redis.GetDatabase(db);
+        }
+    }
+}
