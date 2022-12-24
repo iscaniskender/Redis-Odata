e8da2c3d5fc2add1721c92768f126fef93f6fa90
İskender İşcan <iscaniskender@gmail.com>
2022-12-24 22:59:06 +0300

    OData completed

---
+++ OdataExtention/Program.cs
@@ -0,0 +1,28 @@
+﻿using APIOdata.API.Model;
+using Default;
+
+var serviceRoot = "https://localhost:44383/odata";
+
+var context = new Container(new Uri(serviceRoot));
+
+//var products = context.Products.ExecuteAsync().Result;
+
+//foreach (var product in products.ToList())
+//{
+//    Console.WriteLine(product.Name);
+//}
+
+
+//var categories = context.Categories.AddQueryOption("$filter", "Id eq 2").ExecuteAsync().Result.FirstOrDefault();
+//Console.WriteLine(categories.Name);
+
+
+//var products = context.Products.Expand(x => x.Category);
+
+//products.ToList().ForEach(x => Console.WriteLine(x.Name + "- Category Name: " +x.Category.Name));
+
+
+var categories = context.Categories.AddQueryOption("$select", "Name").ExecuteAsync().Result;
+
+categories.ToList().ForEach(category =>
+    Console.WriteLine(category.Name));
